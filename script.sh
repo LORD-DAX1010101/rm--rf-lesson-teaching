41b7bbdb10e1f8fbdd6981443386172428cd963d
LORD-DAX1010101 <daxtemx1@gmail.com>
2025-08-22 13:09:58 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "⚠️ Running: rm -rf /"
+sleep 1
+echo "Deleting /bin ..."
+sleep 1
+echo "Deleting /etc ..."
+sleep 1
+echo "Deleting /home ..."
+sleep 1
+echo "Deleting /var ..."
+sleep 1
+echo "Deleting /usr ..."
+sleep 1
+echo "💀 System wipe complete (simulation only!)"
